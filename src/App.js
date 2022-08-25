6da910062aff1d93fb44139de636d355f52f51a7
Anshuman  Singh <anshuman.airy04@gmail.com>
2022-08-25 22:46:49 +0530

    global css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./client/styles/GlobalStyle";
 import Routes from "./client/components/Routes";
 import Themes from "./client/styles/theme";
 import Header from "./client/components/Header/Header";
+import "./client/styles/GlobalStyle/global.css";
 
 function App() {
   const [type, setType] = useState("anime");
@@ -13,7 +13,6 @@ function App() {
   return (
     <ThemeProvider theme={Themes}>
       <BrowserRouter>
-        <GlobalStyle />
         <Header type={type} setType={setType} />
         <Routes />
       </BrowserRouter>
